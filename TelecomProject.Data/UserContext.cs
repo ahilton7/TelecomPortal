9cfe12e541b910638fe838ce9151084fa482f33f
hiltona7 <hiltona7@mailbox.winthrop.edu>
2021-10-19 17:57:02 -0400

    Added Entity Classes and Context Object (still need to fix)

---
+++ TelecomProject.Data/UserContext.cs
@@ -0,0 +1,36 @@
+﻿using TelecomProject.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelecomProject.Data
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<Plan> Plans { get; set; }
+
+        public DbSet<Device> Devices { get; set; }
+
+        public UserContext(DbContextOptions options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-RMQDHKI;Initial Catalog=Angular;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+                                           .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+                                           .EnableSensitiveDataLogging();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+}
+}
--- TelecomProject.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TelecomProject.Domain
-{
-    public class Class1
-    {
-    }
-}
