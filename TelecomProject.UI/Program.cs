43558277123d9280c4d1936b1034b6614105533b
Jordan <jordanmatai@gmail.com>
2021-10-26 08:38:11 -0500

    Hot fix for build errors

--- TelecomProject.UI/Program.cs
+++ TelecomProject.UI/Program.cs
@@ -15,30 +15,30 @@ namespace TelecomProject.UI
         
         static void AddData()
         {
-            _context.People.Add(new Person
-            {
-                Name = "Testing2",
-                Password = "Pass2"                
-            });
-            _context.SaveChanges();
-            _context.Plans.Add(new Plan
-            {
-                Device_limit = 5,
-                Cost = 22.99,
-                Name = "Dual plan",
-                PersonId = 1
-            });
-            _context.SaveChanges();
-            _context.Devices.Add(new Device
-            {
-                Phone_type = "Samsung",
-                Phone_number = 1234567899,
-                PersonId = 1,
-                PlanId = 1
-            }); 
+        //    _context.People.Add(new Person
+        //    {
+        //        Name = "Testing2",
+        //        Password = "Pass2"                
+        //    });
+        //    _context.SaveChanges();
+        //    _context.Plans.Add(new Plan
+        //    {
+        //        Device_limit = 5,
+        //        Cost = 22.99,
+        //        Name = "Dual plan",
+        //        PersonId = 1
+        //    });
+        //    _context.SaveChanges();
+        //    _context.Devices.Add(new Device
+        //    {
+        //        Phone_type = "Samsung",
+        //        Phone_number = 1234567899,
+        //        PersonId = 1,
+        //        PlanId = 1
+        //    }); 
 
-            _context.SaveChanges();
-        }
+        //    _context.SaveChanges();
+        //}
         
     }
 }
